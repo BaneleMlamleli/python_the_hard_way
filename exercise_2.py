28b0cc783e5a3a46607f7288f5971b6740bfd1c1
KS_BaneleMlamleli <banele@kineticskunk.com>
2023-07-13 15:46:09 +0200

    Exercise 2 done

---
+++ exercise_2.py
@@ -0,0 +1,5 @@
+# This is my first line comment in Python. Comments are not not compiled
+print("My piece of code to be displayed.")  # and the comment after is ignored
+# You can also use a comment to "disable" or comment out code:
+# print("This won't run.")
+print("This will run as it's not commented code.")
