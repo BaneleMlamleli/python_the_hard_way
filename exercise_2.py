ddd8b037b612835430bb68c771a390e7fc05794e
Banele Mlamleli <banele@kineticskunk.com>
2023-07-18 11:45:21 +0200

    Added comments to different files

--- exercise_2.py
+++ exercise_2.py
@@ -1,3 +1,7 @@
+"""
+learning comments and how to use them
+"""
+
 # This is my first line comment in Python. Comments are not not compiled
 print("My piece of code to be displayed.")  # and the comment after is ignored
 # You can also use a comment to "disable" or comment out code:
