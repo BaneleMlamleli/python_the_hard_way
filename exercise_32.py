b6977b14d77144ea39e33dbe29060334c851da32
Banele Mlamleli <banele@kineticskunk.com>
2023-07-18 14:00:11 +0200

    Exercise 33 done. I also edit the code putting it into a function and having parameters for the maxiimum and increment value so that it's dynamic values.

--- exercise_32.py
+++ exercise_32.py
@@ -1,3 +1,6 @@
+"""
+Loops and list. Creating a list and adding values into the list
+"""
 the_count = [1, 2, 3, 4, 5]
 fruits = ["apples", "oranges", "pears", "apricots"]
 change = [1, "pennies", 2, "dimes", 3, "quarters"]
